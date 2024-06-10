8278ea8db5e68bea859fd6e8e2f11273936f3876
Patrick99sixteen <patrick99sixteen@gmail.com>
2024-06-10 14:21:47 +0700

    Tugas2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+public function index()
+{
+echo "<h1>Perkenalkan</h1>";
+echo"Nama saya Fetrik Gutris
+Saya tingga di daerah Pontianak
+olah raga yang saya sukai adalah
+SepakBola";
+}
+}
